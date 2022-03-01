2caa1a27ee0fe124bbf90215dd27fe2a142f3be0
Atapina <valeria.atapina@mail.ru>
2022-03-01 09:24:26 +0400

    bud fix 2

--- Laba1/Laba1.m
+++ Laba1/Laba1.m
@@ -37,11 +37,13 @@ title("Funtion f(-x + 5)");
 #}
 #8
 #{
+figure(1)
 plot(x(1: end/4), f(1: end/4))
 xlabel("x");
 ylabel("y");
 title("First quarter f(-x)");
 
+figure(2)
 plot(x(end/4: end * 3/4), f(end/4: end * 3/4))
 xlabel("x");
 ylabel("y");
@@ -78,21 +80,25 @@ hold off;
 #}
 #13
 #{
+figure(1)
 plot(x, real(f))
 xlabel("x");
 ylabel("y");
 title("Real part of function f(x)");
 
+figure(2)
 plot(x, imag(f))
 xlabel("x");
 ylabel("y");
 title("Image part of function f(x)");
 
+figure(3)
 plot(x, abs(f))
 xlabel("x");
 ylabel("y");
 title("Amplitude of function f(x)");
 
+figure(4)
 plot(x, arg(f))
 xlabel("x");
 ylabel("y");
@@ -155,32 +161,27 @@ colormap blue;
 #}
 #17
 R = sqrt(X.^2 + Y.^2);
-FI = atan2(X, Y);
+FI = atan2(Y, X);
 
 #18
-fr = airy(1, R);
+fr = airy(1, -R);
 H = fr.* exp(1i*4*FI);
 #{
 figure(10);
 imagesc(real(H));
 colorbar;
-colormap blue;
-#}
-#{
+
 figure(11);
 imagesc(imag(H));
-#}
-#{
+colorbar;
+
 figure(12);
 imagesc(abs(H));
 colorbar;
-colormap blue;
-#}
-#{
+
 figure(13);
 imagesc(arg(H));
 colorbar;
-colormap blue;
 #}
 #19
 middle = H(end/2, :);
